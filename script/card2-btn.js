b7214b4c0d782f0c11e063e79aab0e89aa31867c
eishadi <eftajulislam.sadi@gmail.com>
2025-03-02 23:04:08 +0600

    Btn Acctivity Added

---
+++ script/card2-btn.js
@@ -0,0 +1,27 @@
+document.getElementById("card2-btn")
+.addEventListener("click", function(event){
+    alert("Board Updated Successfully")
+    // Task Assigned
+    const taskAssigned = document.getElementById("task-assigned").innerText;
+    const convertedTaskAssigned = parseInt(taskAssigned);
+    const sum = convertedTaskAssigned - 1;
+    document.getElementById("task-assigned").innerText = sum;
+    // Task Completed
+    const taskCompleted = document.getElementById("task-completed").innerText;
+    const convertedTaskCompleted = parseInt(taskCompleted);
+    const add = convertedTaskCompleted + 1;
+    document.getElementById("task-completed").innerText = add;
+     // Add Current Time
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    // Msg Added
+    const msgContainer = document.getElementById("msg-container");
+    const p = document.createElement('p');
+    p.className = "w-[318px] font-semibold mx-auto rounded-md p-3 mt-6 mb-7 bg-blue-50";
+    p.innerText =  `You have Completed The Task Add Dark Mode at ${timeString}`;
+    msgContainer.appendChild(p);
+    // BTN Disabled
+    document.getElementById("card2-btn").style.color = "black"
+    document.getElementById("card2-btn").style.background = "gray"
+    document.getElementById("ccard2-btn").disabled = true;
+})
---
+++ script/card3-btn.js
@@ -0,0 +1,27 @@
+document.getElementById("card3-btn")
+.addEventListener("click", function(event){
+    alert("Board Updated Successfully")
+    // Task Assigned
+    const taskAssigned = document.getElementById("task-assigned").innerText;
+    const convertedTaskAssigned = parseInt(taskAssigned);
+    const sum = convertedTaskAssigned - 1;
+    document.getElementById("task-assigned").innerText = sum;
+    // Task Completed
+    const taskCompleted = document.getElementById("task-completed").innerText;
+    const convertedTaskCompleted = parseInt(taskCompleted);
+    const add = convertedTaskCompleted + 1;
+    document.getElementById("task-completed").innerText = add;
+     // Add Current Time
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    // Msg Added
+    const msgContainer = document.getElementById("msg-container");
+    const p = document.createElement('p');
+    p.className = "w-[318px] font-semibold mx-auto rounded-md p-3 mt-6 mb-7 bg-blue-50";
+    p.innerText =  `You have Completed The Task Optimize Home Page at ${timeString}`;
+    msgContainer.appendChild(p);
+    // BTN Disabled
+    document.getElementById("card3-btn").style.color = "black"
+    document.getElementById("card3-btn").style.background = "gray"
+    document.getElementById("card3-btn").disabled = true;
+})
